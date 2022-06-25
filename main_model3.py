405f9ebed3792cb80c8634698fbba07ab9c6732f
normanlapid <normanlapid@gmail.com>
2022-06-25 18:47:12 +0800

    Update main_model3.py

--- main_model3.py
+++ main_model3.py
@@ -16,3 +16,18 @@ scaler_filename = 'robustscaler_3.sav'
 scaler = pickle.load(open(scaler_filename, 'rb'))
 
 st.header("Model 3: Predicting Final GWA for Business Students")
+
+st.subheader("Please input relevant features for the student:")
+
+st.text("Student Grades:")
+
+input_eng1 = st.number_input("ENG 1 (PC)", min_value=1.00, max_value=5.00, step=0.01)
+input_math1 = st.number_input("MATH 1 (MMW)", min_value=1.00, max_value=5.00, step=0.01)
+input_cw1 = st.number_input("CW 1", min_value=1.00, max_value=5.00, step=0.01)
+input_ba1 = st.number_input("BA 1 (HRM)", min_value=1.00, max_value=5.00, step=0.01)
+input_cbmec1 = st.number_input("CBMEC 1", min_value=1.00, max_value=5.00, step=0.01)
+input_cbmec2 = st.number_input("CBMEC 2", min_value=1.00, max_value=5.00, step=0.01)
+input_eco1 = st.number_input("ECO 1 (BM)", min_value=1.00, max_value=5.00, step=0.01)
+input_ba2 = st.number_input("BA 2 (MA)", min_value=1.00, max_value=5.00, step=0.01)
+
+
